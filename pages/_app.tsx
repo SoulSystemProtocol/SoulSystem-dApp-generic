14a428bb4e270fec9866d465265f3dc847a1bacf
Asiya <asya.asha@gmail.com>
2022-07-20 10:44:32 +0200

    [ui] add sidenav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,29 @@
 import { DataProvider } from "contexts/data";
 import { Web3Provider } from "contexts/web3";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import type { AppProps } from "next/app";
 import { SnackbarProvider } from "notistack";
 import "../styles/globals.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 /**
  * Component with an app.
  */
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <Web3Provider>
-        <DataProvider>
-          <Component {...pageProps} />
-        </DataProvider>
-      </Web3Provider>
-    </SnackbarProvider>
+    <ThemeProvider theme={darkTheme}>
+      <SnackbarProvider maxSnack={3}>
+        <Web3Provider>
+          <DataProvider>
+            <Component {...pageProps} />
+          </DataProvider>
+        </Web3Provider>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
