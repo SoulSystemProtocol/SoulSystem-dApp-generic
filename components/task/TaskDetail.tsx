40c2a1850ecbf7413ea803d1f041a4833a96c8f7
kiv1n <aloha@kiv1n.ru>
2022-07-24 19:34:11 +0300

    Display stage on the task detail component

--- components/task/TaskDetail.tsx
+++ components/task/TaskDetail.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
+import { taskStageToString } from 'utils/converters';
 import EntityImage from '../entity/EntityImage';
 
 /**
@@ -19,7 +20,12 @@ export default function TaskDetail({ item, sx }: any) {
           <EntityImage item={item.game} />
         </Box>
         <Box sx={{ mt: { xs: 2, md: 0 }, ml: { md: 4 } }}>
-          <Typography variant="h4">{item.name}</Typography>
+          <Typography color="text.secondary" variant="body2">
+            {taskStageToString(item.stage)}
+          </Typography>
+          <Typography variant="h4" sx={{ mt: 2.5 }}>
+            {item.name}
+          </Typography>
           <Typography sx={{ mt: 1 }}>{item.uriData?.description}</Typography>
         </Box>
       </Box>
