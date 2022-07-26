561df2f2541cecaee1c22bfa64da1d06f8e0a5ac
Roy <toledoroy@gmail.com>
2022-07-26 14:40:18 -0700

    Update AddressHash.tsx

--- components/web3/AddressHash.tsx
+++ components/web3/AddressHash.tsx
@@ -1,7 +1,12 @@
 import { addressToShortAddress } from 'utils/converters';
 import { CopyIcon } from 'components/icons';
 
-export default function AddressHash(address: string) {
+export default function AddressHash({
+  address,
+}: {
+  address: string;
+}): JSX.Element {
+  if (!address) return <></>;
   return (
     <>
       {addressToShortAddress(address)}
