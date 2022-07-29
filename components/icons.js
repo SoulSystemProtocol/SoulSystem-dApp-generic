f5267061635cec11048dd5eff2ad7c01437c274c
Roy <toledoroy@gmail.com>
2022-07-29 15:47:32 -0700

    Update icons.js

--- components/icons.js
+++ components/icons.js
@@ -16,6 +16,6 @@ export const CopyIcon = (args) => (
     <path d="M15 3v4a1 1 0 0 0 1 1h4" />
     <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
     <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
-    <title id="copy-address">Copy Address</title>
+    {/* <title id="copy-address">Copy Address</title> */}
   </svg>
 );
