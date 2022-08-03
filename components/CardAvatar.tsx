4db8f38036974addf56681d3029b3a3a6fbb7e6f
Roy <toledoroy@gmail.com>
2022-08-03 10:53:08 -0700

    [FIX] Missing Image Placehodlers

--- components/CardAvatar.tsx
+++ components/CardAvatar.tsx
@@ -1,35 +1,31 @@
-/**
- *  Card Avatar component.
- */
 import { SchoolOutlined } from '@mui/icons-material';
 import { Avatar } from '@mui/material';
 import { Box } from '@mui/system';
 import Link from 'next/link';
 
+/**
+ *  Component: Card Avatar Image
+ */
 export default function CardAvatar({ imgSrc, avatarIcon, link, sx }: any) {
-  if (imgSrc) {
-    const renderAvatar = (
-      <Avatar
-        sx={{
-          cursor: 'pointer',
-          width: 82,
-          height: 82,
-          borderRadius: '16px',
-        }}
-        src={imgSrc}
-      >
-        {avatarIcon ? avatarIcon : <SchoolOutlined />}
-      </Avatar>
-    );
+  const renderAvatar = (
+    <Avatar
+      sx={{
+        cursor: 'pointer',
+        width: 82,
+        height: 82,
+        borderRadius: '16px',
+      }}
+      src={imgSrc}
+    >
+      {avatarIcon ? avatarIcon : <SchoolOutlined />}
+    </Avatar>
+  );
 
-    const renderAvatarLink = link && (
-      <Link href={link} passHref>
-        {renderAvatar}
-      </Link>
-    );
+  const renderAvatarLink = link && (
+    <Link href={link} passHref>
+      {renderAvatar}
+    </Link>
+  );
 
-    return <Box sx={{ ...sx }}>{renderAvatarLink || renderAvatar}</Box>;
-  }
-
-  return null;
+  return <Box sx={{ ...sx }}>{renderAvatarLink || renderAvatar}</Box>;
 }
