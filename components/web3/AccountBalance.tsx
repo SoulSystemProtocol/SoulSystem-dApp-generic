1d7cf2b7f54edccf936446bb223e5da1940705c4
Roy <toledoroy@gmail.com>
2022-08-02 17:29:52 -0700

    [FIX] Account for missing provider and address changes

--- components/web3/AccountBalance.tsx
+++ components/web3/AccountBalance.tsx
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import { Web3Context } from 'contexts/web3';
 import useError from 'hooks/useError';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 /**
  * A component Account's Balance
@@ -10,16 +10,22 @@ export default function AccountBalance({ address }: any) {
   const [balance, setBalance] = useState('');
   const { provider } = useContext(Web3Context);
   const { handleError } = useError();
-  if (address) {
-    provider
-      .getBalance(address)
-      .then((balance: any) => {
-        // convert a currency unit from wei to ether
-        const balanceInEth = ethers.utils.formatEther(balance);
-        console.log(`balance: ${balanceInEth} ETH`);
-        setBalance(balanceInEth);
-      })
-      .catch((error: Error) => handleError(error, true));
-  }
+
+  useEffect(() => {
+    if (address && provider) {
+      provider
+        .getBalance(address)
+        .then((balance: any) => {
+          // convert a currency unit from wei to ether
+          const balanceInEth = ethers?.utils.formatEther(balance);
+          // console.log(`balance: ${balanceInEth} ETH`);
+          setBalance(balanceInEth);
+        })
+        .catch((error: Error) => handleError(error, true));
+    } else setBalance('?');
+  }, [address, provider]);
+
+  console.log(`get balance:`, address);
+
   return <>{balance}</>;
 }
