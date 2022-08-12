6b2dd6a6449f5a7ba2eced7f96f20bf48af7105e
Roy <toledoroy@gmail.com>
2022-08-12 12:49:02 -0700

    Infura private gateway

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -13,15 +13,19 @@ export function getTraitValue(
 
 /**
  * Resolve IPFS Links
+ *
+ * https://dweb.link/ipfs/
+ * https://cloudflare-ipfs.com/ipfs/
+ * https://gateway.ipfs.io/ipfs/
+ * https://ipfs.moralis.io:2053/ipfs/
  */
-// export const resolveLink = (url: any) => (!url || !url.includes("ipfs://")) ? url : url.replace("ipfs://", "https://gateway.ipfs.io/ipfs/");
-// export const resolveLink = (url: any) => (!url || !url.includes("ipfs://")) ? url : url.replace("ipfs://", "https://ipfs.moralis.io:2053/ipfs/");
-// export const resolveLink = (url: any) => (!url || !url.includes("ipfs://")) ? url : url.replace("ipfs://", "https://cloudflare-ipfs.com/ipfs/");
 export const resolveLink = (url: any) => {
   //[FIX] Remove Hardcoded Infura gateway
   if (!!url) url = url.replace('https://ipfs.infura.io/ipfs/', 'ipfs://');
   return !url || !url.includes('ipfs://')
     ? url
-    : url.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/');
+    : url.replace('ipfs://', 'https://grid.infura-ipfs.io/ipfs/');
+  // : url.replace('ipfs://', 'https://gateway.ipfs.io/ipfs/');
+  // : url.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/');
   // : url.replace('ipfs://', 'https://ipfs.infura.io/ipfs/'); //Error: "Public Gateway Is Not Supported Anymore - Setup a Dedicated Gateway"
-}
+};
