0dfca8ddade6fc01b967799f8112e05fb8a95eb6
Roy <toledoroy@gmail.com>
2022-09-19 13:12:01 -0700

    Prettier

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -33,13 +33,13 @@ const top_links: any = [
     route: 'erc',
     hide: process.env.NEXT_PUBLIC_FEATURE_NFT == 'false',
     //TODO: Support Hiding
- },
- {
-    label: 'Event Explorer', 
+  },
+  {
+    label: 'Event Explorer',
     route: 'explorer',
     hide: process.env.NEXT_PUBLIC_FEATURE_EXPLORER == 'false',
     //TODO: Support Hiding
- },
+  },
 ];
 
 //Define Sidemenu Links
