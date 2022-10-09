dff3672a174b10e0d2128d033d2af29f4112ff8a
Roy <toledoroy@gmail.com>
2022-10-08 19:31:40 -0700

    processCardContent Function

--- utils/cardContents.tsx
+++ utils/cardContents.tsx
@@ -1,24 +1,13 @@
-import { PersonOutlineOutlined } from '@mui/icons-material';
+import {
+  PersonOutlineOutlined,
+  WorkOutlineOutlined,
+} from '@mui/icons-material';
 import {
   addressToShortAddress,
   hexStringToJson,
   soulToFirstLastNameString,
 } from './converters';
 import { resolveLink } from './metadata';
-
-// Item Processing Function
-export const gameCardContent = (item: any) => {
-  let metadata = hexStringToJson(item.uriData);
-  let ret = {
-    id: item.id,
-    imgSrc: resolveLink(metadata.image),
-    label: metadata?.description,
-    title: metadata?.name,
-    metadata: metadata,
-    link: `/game/${item.owner}`,
-  };
-  return ret;
-};
 
 // Item Processing Function
 export const soulCardContent = (item: any) => {
@@ -42,6 +31,38 @@ export const soulCardContent = (item: any) => {
   return ret;
 };
 
+// Game Card Processing
+export const gameCardContent = (item: any) => {
+  let metadata = hexStringToJson(item.uriData);
+  let ret = {
+    id: item.id,
+    imgSrc: resolveLink(metadata.image),
+    label: metadata?.description,
+    title: metadata?.name,
+    metadata,
+    link: `/game/${item.owner}`,
+    avatarIcon: <WorkOutlineOutlined />,
+  };
+  // ret.avatarIcon = (<WorkOutlineOutlined />);
+  return ret;
+};
+
+// Process Card Processing
+export const processCardContent = (item: any) => {
+  let metadata = hexStringToJson(item.uriData);
+  let ret = {
+    id: item.id,
+    imgSrc: resolveLink(metadata.image),
+    avatarIcon: <WorkOutlineOutlined />,
+    label: metadata?.description,
+    title: metadata?.name,
+    metadata,
+    link: `/task/${item.owner}`,
+  };
+  console.log('Task Item', { ret, item });
+  return ret;
+};
+
 // Game Participant
 export const gamePartCardContent = (item: any) => {
   let metadata = hexStringToJson(item.entity.metadata);
@@ -50,7 +71,7 @@ export const gamePartCardContent = (item: any) => {
     imgSrc: resolveLink(metadata.image),
     label: metadata?.description,
     title: metadata?.name,
-    metadata: metadata,
+    metadata,
     link: `/game/${item.entity.id}`,
   };
   // console.log('[DEV] daoPartCardContent() item', { item, ret });
