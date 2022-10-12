a68bdea865c01d871c6d6a787db31cb3aa559fdd
Roy <toledoroy@gmail.com>
2022-10-11 20:21:10 -0700

    Display POAP from POAP API

---
+++ components/web3/DisplayPOAP.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react';
+import { Avatar, Box, Grid, Stack, Tooltip, Typography } from '@mui/material';
+import useError from 'hooks/useError';
+import { SupervisedUserCircle } from '@mui/icons-material';
+// import Link from 'components/utils/Link';
+// import { addressToShortAddress } from 'utils/converters';
+
+/**
+ * Component: Display POAP
+ */
+export default function DisplayPOAP({ account, sx = {} }: any): JSX.Element {
+  const { handleError } = useError();
+  const [items, setItems] = useState<Array<any>>([]);
+  const [isShowMore, setIsShowMore] = useState<boolean>(false);
+
+  //Load POAPs
+  async function getPOAP(address: string): Promise<void> {
+    const options: any = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'X-API-Key': process.env.NEXT_PUBLIC_POAP_API_KEY,
+      },
+    };
+    try {
+      const response = await fetch(
+        `https://api.poap.tech/actions/scan/${address}`,
+        options,
+      ).then((response) => response.json());
+      // .then((response) => console.log(response))
+      // .catch((err) => console.error(err));
+      console.log('POAP Res:', { account, response });
+      setItems(response);
+    } catch (error: any) {
+      setItems([]);
+      console.error('POAP Fetch Failed:', { error, address });
+      // throw new Error(`POAP Fetch Failed: ${error}`);
+      handleError(
+        {
+          message: `POAP Fetch Failed: ${error}`,
+          name: 'POAP API Failed',
+        },
+        true,
+      );
+    }
+  }
+
+  useEffect(() => {
+    //Load POAPs
+    account ? getPOAP(account) : setItems([]);
+  }, [account]);
+
+  if (!account) return <></>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        ...sx,
+      }}
+    >
+      <Grid container spacing={2} sx={{ ...sx }}>
+        {!items && (
+          <Grid item xs={12}>
+            <Typography>Loading...</Typography>
+          </Grid>
+        )}
+        {items?.length === 0 && (
+          <Grid item xs={12}>
+            <Typography>
+              {/* No POAPs for {addressToShortAddress(account)} */}
+            </Typography>
+          </Grid>
+        )}
+        {items?.length > 0 && (
+          <>
+            {items.map((item: any) => (
+              <Grid key={item.event.id} item xs={12} md={1}>
+                <Tooltip
+                  title={`${item.event.name} - ${item.event.description}`}
+                >
+                  <Stack direction="column" spacing={1} sx={{ ...sx }}>
+                    <Avatar
+                      alt={item.event.name}
+                      sx={{
+                        width: '96%',
+                        height: '96%',
+                        borderRadius: '12%',
+                        ...sx,
+                      }}
+                      src={item?.event?.image_url}
+                    >
+                      <SupervisedUserCircle />
+                    </Avatar>
+                    <Box>
+                      {/* <Link href={`/POAP/${item.id}`}> */}
+                      <Typography variant="body1">{item.name}</Typography>
+                      {/* </Link> */}
+                    </Box>
+                  </Stack>
+                </Tooltip>
+              </Grid>
+            ))}
+          </>
+        )}
+      </Grid>
+    </Box>
+  );
+}
