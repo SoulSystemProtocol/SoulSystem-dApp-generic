c7b0e69171c00427e92f80358dff675c34a9dc5a
Roy <toledoroy@gmail.com>
2022-10-12 11:28:29 -0700

    Update useError.ts

--- hooks/useError.ts
+++ hooks/useError.ts
@@ -6,14 +6,14 @@ import useToast from './useToast';
 export default function useError() {
   const { showToastError } = useToast();
 
-  let handleError = function (error: Error, isErrorToastRequired: boolean) {
+  let handleError = function (error: any, isErrorToastRequired: boolean) {
     //To Console
     console.error(error);
     //** Custom Exceptions
     // eslint-disable-next-line prettier/prettier
     if (typeof error == 'object' && error.message == 'Internal JSON-RPC error.') isErrorToastRequired = false;
     // eslint-disable-next-line prettier/prettier
-    if (typeof error == 'object' && error.message == 'MetaMask Tx Signature: User denied transaction signature.') {
+    else if (typeof error == 'object' && error?.code == "ACTION_REJECTED") {
       isErrorToastRequired = false;
     }
     //For User
