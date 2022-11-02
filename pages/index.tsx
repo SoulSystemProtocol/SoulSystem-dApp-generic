e2e516e6766e1a025d3fafdad8369660f5cd5a02
Roy <toledoroy@gmail.com>
2022-11-02 14:00:48 -0700

    Updated Homepage Text

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,11 @@ import { Web3Context } from 'contexts/Web3Context';
 import { APP_CONFIGS } from '../constants';
 import { getPageTitle } from '../utils';
 import Layout from 'components/layout/Layout';
+import ConnectButton from 'components/web3/connect/ConnectButton';
 
 const wrapperStyle = {
   // background: 'black',
   color: '#fff',
-  minHeight: '100vh',
-  mt: 6,
   textAlign: 'center',
   px: { xs: 2, md: 4 },
   pt: { xs: 2, md: 4 },
@@ -25,36 +24,43 @@ export default function Landing() {
 
   return (
     <Layout title={getPageTitle('Home')}>
-      <Box sx={{ width: '100%' }}>
+      <Box sx={{ width: '100%', minHeight: '100vh' }}>
         <Box sx={wrapperStyle}>
-          <Typography mt={6} gutterBottom variant="h1" fontWeight={700}>
+          <Typography
+            gutterBottom
+            variant="h1"
+            fontWeight={700}
+            fontSize="5rem"
+            letterSpacing="0.1em"
+          >
             {APP_CONFIGS.NAME}
           </Typography>
-          <Typography variant="h4" fontSize="2em" letterSpacing="0.25em">
-            On-chain bounties
+          <Typography variant="h4" fontSize="2em" letterSpacing="0.05em">
+            On-chain low-code framework for structuring socio-economic systems
           </Typography>
-          <Typography
-            variant="h4"
-            fontSize="1.85em"
-            letterSpacing="0.3em"
-            sx={{ mt: 4, mb: 4 }}
+        </Box>
+
+        {!account && (
+          <Box mt={4} textAlign="center">
+            <Typography fontSize="1.2em" letterSpacing="0.02em">
+              Connect wallet to explore mDAOs <ConnectButton sx={{ ml: 4 }} />
+            </Typography>
+          </Box>
+        )}
+        {account && (
+          <Box
+            sx={{
+              mt: 3,
+              display: 'flex',
+              justifyContent: 'center',
+              border: '1px solid #333',
+              borderRadius: 2,
+            }}
           >
-            The future of work
-          </Typography>
-          <Typography variant="h4" fontSize="1.6em" sx={{ mt: 1 }}>
-            The uprising of the Gig Economy
-          </Typography>
-          {!account && (
-            <Box mt={2}>
-              <Typography fontSize="1.2em" letterSpacing="0.08em">
-                Connect wallet to explore mDAOs
-              </Typography>
-            </Box>
-          )}
-          <Box sx={{ mt: 18, display: 'flex', justifyContent: 'center' }}>
             <Typography variant="h6" mr={1}>
               Need some test tokens?
             </Typography>
+            <br />
             <Typography sx={{ marginTop: '5px' }}>
               Request some from the{' '}
               <Link href={`https://faucet.polygon.technology/`} passHref>
@@ -69,6 +75,34 @@ export default function Landing() {
               </Link> */}
             </Typography>
           </Box>
+        )}
+
+        <Typography variant="h4" sx={{ mt: 4 }}>
+          [WIP] This is a demo dApp for the solidify low-code protocol
+        </Typography>
+        <Box>
+          <Typography variant="h4" sx={{ mt: 4 }}>
+            Featuring soul-systems - a cluster of composible primitives
+            structured around a soulbound contract that allows you to
+          </Typography>
+          <ul>
+            <li>Mint SBT profiles as wallet abstraction</li>
+            <li>Create Games as a context containing SBT profiles</li>
+            <li>Set up Rules and design an interaction flow</li>
+            <li>
+              Create automated incentives and attach them to the rules of the
+              context
+            </li>
+            <li>
+              Modarate your soul-system. Ban bad actors, and recover lost or
+              stolen assets (Safe NFTs)
+            </li>
+            <li>Track experience & reputation</li>
+            <li>
+              Extend games with customized affordences that fit your specific
+              requirements with minimal development effort
+            </li>
+          </ul>
         </Box>
       </Box>
     </Layout>
