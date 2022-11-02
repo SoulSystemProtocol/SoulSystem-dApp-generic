11ddfe0386027024629a322b367072945291cad1
Roy <toledoroy@gmail.com>
2022-11-02 13:11:31 -0700

    Tidy up

--- hooks/useIpfs.ts
+++ hooks/useIpfs.ts
@@ -1,17 +1,19 @@
 // import axios from 'axios';
 import { create } from 'ipfs-http-client';
 
-/** DEPRECATE - If it doesn't keep a state it doesn't need to be a hook
+/** DEPRECATE - If it doesn't have a state it doesn't need to be a hook
  * Hook for work with IPFS.
  */
 export default function useIpfs() {
-  // eslint-disable-next-line prettier/prettier
-  const auth = 'Basic ' + Buffer.from(process.env.NEXT_PUBLIC_INFURA_PROJECT_ID + ':' + process.env.NEXT_PUBLIC_INFURA_SECRET).toString('base64');
+  //Validate
   if (
     !process.env.NEXT_PUBLIC_INFURA_PROJECT_ID ||
     !process.env.NEXT_PUBLIC_INFURA_SECRET
   )
     console.error('Missing Infura API Data');
+
+  // eslint-disable-next-line prettier/prettier
+  const auth = 'Basic ' + Buffer.from(process.env.NEXT_PUBLIC_INFURA_PROJECT_ID + ':' + process.env.NEXT_PUBLIC_INFURA_SECRET).toString('base64');
   const infuraClient = create({
     // url: process.env.NEXT_PUBLIC_INFURA_IPFS_API,
     host: 'ipfs.infura.io',
@@ -25,7 +27,7 @@ export default function useIpfs() {
     url: process.env.NEXT_PUBLIC_THE_GRAPH_IPFS_API,
   });
 
-  let uploadFileToIPFS = async function (file: any) {
+  const uploadFileToIPFS = async function (file: any) {
     const created = await infuraClient.add({
       path: '',
       content: file,
@@ -35,7 +37,7 @@ export default function useIpfs() {
     return { cid, url };
   };
 
-  let uploadJsonToIPFS = async function (json: any) {
+  const uploadJsonToIPFS = async function (json: any) {
     const jsonString = JSON.stringify(json);
     // Upload to the graph for usage in graph queries
     theGraphClient
@@ -48,15 +50,6 @@ export default function useIpfs() {
     const url = `ipfs://${cid}`;
     return { cid, url };
   };
-  /* MOVED TO Helper
-  let loadJsonFromIPFS = async function (url: any) {
-    const response = await axios.get(url);
-    if (response.data.errors) {
-      throw new Error(`Error loading json from IPFS: ${response.data.errors}`);
-    }
-    return response.data;
-  };
-  //, loadJsonFromIPFS
-  */
+
   return { uploadFileToIPFS, uploadJsonToIPFS };
 }
