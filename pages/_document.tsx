005d338226d6f65a299b86f48581290cb81b5a1d
Roy <toledoroy@gmail.com>
2022-11-03 09:55:43 -0700

    Added missing font includes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
