d847aa370ed5508fc17a7d30701e280beed0b119
Roy <toledoroy@gmail.com>
2022-12-17 10:49:31 -0800

    Debugging Vercel Deployment...

--- components/entity/soul/SocialLinks.tsx
+++ components/entity/soul/SocialLinks.tsx
@@ -1,8 +1,8 @@
 import { MailOutlineRounded } from '@mui/icons-material';
 import { Stack } from '@mui/material';
 import Link from 'components/utils/Link';
-import { PROFILE_TRAITS, Trait } from './ProfileTraits';
 import { AttributeHelper } from 'helpers/AttributeHelper';
+import { PROFILE_TRAITS, Trait } from './ProfileTraits';
 
 export default function SocialLinks({ soul, sx }: any): JSX.Element {
   const email = AttributeHelper.extractValue(
