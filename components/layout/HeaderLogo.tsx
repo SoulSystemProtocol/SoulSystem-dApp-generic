47091df4e24b10c4032d9e27babcbdc86daae168
Roy <toledoroy@gmail.com>
2022-12-30 11:16:10 -0800

    Update HeaderLogo.tsx

--- components/layout/HeaderLogo.tsx
+++ components/layout/HeaderLogo.tsx
@@ -27,7 +27,7 @@ export default function HeaderLogo(): JSX.Element {
       >
         {process.env.NEXT_PUBLIC_FEATURE_DEV
           ? 'v' + process.env.NEXT_PUBLIC_VERSION
-          : 'beta'}
+          : 'alpha'}
       </Typography>
     </Box>
   );
