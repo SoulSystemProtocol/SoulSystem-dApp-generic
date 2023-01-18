c3ce920510c085bd038d050bd11ba82cc5a4b624
Roy <toledoroy@gmail.com>
2023-01-18 13:32:34 -0800

    Prettier

--- hooks/useTask.ts
+++ hooks/useTask.ts
@@ -6,31 +6,27 @@ import useSubgraph from './useSubgraph';
 export default function useTask() {
   const { findClaims } = useSubgraph();
 
-  const getTaskById = async function (id: string): Promise<any | null> {
+  const getTaskById = async (id: string): Promise<any | null> => {
     const items = await getTasks([id], undefined);
     return items.length > 0 ? items[0] : null;
   };
 
-  const getTasks = async function (
+  const getTasks = async (
     ids?: Array<string>,
     type?: string,
     projectId?: string,
     first = 10,
     skip = 0,
-  ): Promise<Array<any>> {
+  ): Promise<Array<any>> => {
     const subgraphTasks = await findClaims(ids, type, projectId, first, skip);
     return subgraphTasks;
   };
 
-  const isSoulHasRole = function (
-    task: any,
-    soul: string,
-    roleId: string,
-  ): boolean {
+  const isSoulHasRole = (task: any, soul: string, roleId: string): boolean => {
     return getSoulsByRole(task, roleId).includes(soul);
   };
 
-  const getSoulsByRole = function (task: any, roleId: string): Array<string> {
+  const getSoulsByRole = (task: any, roleId: string): Array<string> => {
     const taskRole = task.roles?.find(
       (element: any) => element?.roleId === roleId,
     );
