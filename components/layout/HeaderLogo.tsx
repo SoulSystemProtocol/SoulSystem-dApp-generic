35638a444b3281e1493de552b1d269e2ee155153
Roy <toledoroy@gmail.com>
2023-06-26 00:11:19 -0700

    [FIX] parameter checks

--- components/layout/HeaderLogo.tsx
+++ components/layout/HeaderLogo.tsx
@@ -28,7 +28,7 @@ export default function HeaderLogo(): JSX.Element {
       >
         {process.env.NEXT_PUBLIC_FEATURE_DEV
           ? 'v' + process.env.NEXT_PUBLIC_VERSION
-          : !getChainData().live
+          : !getChainData()?.live
           ? 'testnet'
           : ''}
       </Typography>
