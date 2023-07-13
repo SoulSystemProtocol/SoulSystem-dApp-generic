8c83b73cc5df7e4854a08d509e018b1409327b1b
Roy <toledoroy@gmail.com>
2023-07-12 23:04:23 -0700

    Basic Post Link Display

--- components/entity/post/PostSingleDisplay.tsx
+++ components/entity/post/PostSingleDisplay.tsx
@@ -1,5 +1,6 @@
-import { Grid, Paper, Stack, Chip, Typography } from '@mui/material';
+import { Grid, Paper, Stack, Chip, Typography, Box } from '@mui/material';
 import SoulCompactCard from 'components/entity/soul/SoulCompactCard';
+import Link from 'components/utils/Link';
 
 export default function PostSingleDisplay({
   post,
@@ -8,6 +9,8 @@ export default function PostSingleDisplay({
   post: any;
   children?: JSX.Element;
 }): JSX.Element {
+  console.log('post', post);
+
   return (
     <Grid item key={post.id} xs={12}>
       <Paper sx={{ p: 2 }}>
@@ -19,17 +22,24 @@ export default function PostSingleDisplay({
         {/* Message */}
         <Paper variant="outlined" sx={{ p: 2, mt: 1 }}>
           <Stack direction="row" spacing={1} justifyContent="space-between">
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 'normal',
-                overflow: 'hidden',
-                fontSize: '0.8rem',
-              }}
-              gutterBottom
-            >
-              {post?.metadata?.text}
-            </Typography>
+            <Box>
+              <Typography
+                variant="body2"
+                sx={{
+                  fontWeight: 'normal',
+                  overflow: 'hidden',
+                  fontSize: '0.8rem',
+                }}
+                gutterBottom
+              >
+                {post?.metadata?.text}
+              </Typography>
+              {post?.metadata?.link && (
+                <Typography variant="body2" sx={{ fontSize: '0.7em', mb: 1 }}>
+                  <Link target="_blank" href={post.metadata.link}>{post.metadata.link}</Link>
+                </Typography>
+              )}
+            </Box>
             {children}
           </Stack>
           <Typography variant="body2" sx={{ fontSize: '0.7em', opacity: 0.66 }}>
